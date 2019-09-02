98ddf2b61dfce6b7c23d1156e4e44ace1803a2fb
Ugur Guney <ugurguney@gmail.com>
2019-09-02 18:34:12 +0000

    script to download Pride and Prejucide.
    Also keep the text out of source code repo.

---
+++ bin/download_pride_and_prejudice.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd ../texts
+wget https://raw.githubusercontent.com/crista/exercises-in-programming-style/master/pride-and-prejudice.txt
+mv pride-and-prejudice.txt pride_and_prejudice.txt
